ea89a886d596263ba999d22b3046b09441cb0334
sshniro <sshniro@gmail.com>
2019-07-23 00:02:28 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Example from './upload';
+
+function App() {
+  return (
+    <div >
+        <Example/>
+    </div>
+  );
+}
+
+export default App;
